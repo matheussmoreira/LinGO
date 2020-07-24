598b3a2054ab2d28644476c596c9f2ce33745e22
Matheus S. Moreira <matheussmoreira100@gmail.com>
2020-07-24 14:26:15 -0300

    Initial Commit

---
+++ BarreirasLinguisticas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarreirasLinguisticas
+//
+//  Created by Matheus S. Moreira on 24/07/20.
+//  Copyright © 2020 Matheus S. Moreira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
