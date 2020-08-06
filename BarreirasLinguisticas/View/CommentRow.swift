7cb133b5114e9d49aeef4a3ab27a1dd63f2d7392
Victor Duarte <victorduarte.contato@gmail.com>
2020-08-06 14:28:12 -0300

    Modificação no comentário com a CommentRow.

--- BarreirasLinguisticas/View/CommentRow.swift
+++ BarreirasLinguisticas/View/CommentRow.swift
@@ -14,45 +14,51 @@ struct CommentRow: View {
         
         ZStack {
             
-            RoundedRectangle(cornerRadius: 15, style: .continuous)
+           /*RoundedRectangle(cornerRadius: 15, style: .continuous)
                 .fill(Color.white)
                 .frame(height: 80)
                 .shadow(radius: 8)
                 .padding()
-            
+            */
             VStack {
                 HStack {
                     Image("foto_leo")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
-                        .frame(width: 30.0, height: 30.0)
+                        .frame(width: 40.0, height: 40.0)
                         .clipShape(Circle())
                         .padding(.leading)
                     
-                    Text("Leonardo da Vinci")
-                        .fontWeight(.bold)
-                    
-                    Spacer()
-                    HStack {
-                        Text("Fluencia")
-                            .foregroundColor(.gray)
-                            .font(.footnote)
-                        Circle()
-//                          .fill(comentario.publicador.usuario.cor_fluencia)
-                            .frame(width: 10.0, height: 10.0)
-                            .padding(.trailing)
-                    }
-                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Leonardo da Vinci")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .lineLimit(1)
+                            
+                            Spacer()
+                            Text("English Level")
+                                .foregroundColor(.gray)
+                                .font(.footnote)
+                            Circle()
+                                .frame(width: 10.0, height: 10.0)
+                                .padding(.trailing)
+                        }
+                        Text("Apple Developer Academy | PUC-Rio | Brazil")
+                            .font(.caption)
+                            .foregroundColor(Color.gray)
+                            .lineLimit(1)
+                    } //VStack
                 } //HStack
-                HStack {
-                    Text("usahusha")
+              
+                Text("We are the world, we are the children... we are the ones who make a brighter day, so lets got given. That's a choice we're making, to be saving our own lives.")
+                    .multilineTextAlignment(.leading)
                         .padding(.horizontal)
+                        .frame(height: 100.0)
                         
-                    Spacer()
-                }
             } //VStack
             .padding(.horizontal)
-        } //ZStack
+        }//ZStack
     }
 }
 
