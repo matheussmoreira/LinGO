d14d6a4b5ad8936c06f92b94de95e60e672a2edd
Victor Duarte <victorduarte.contato@gmail.com>
2020-08-06 18:27:35 -0300

    Alterações em comentários e questões.

--- BarreirasLinguisticas/View/CommentRow.swift
+++ BarreirasLinguisticas/View/CommentRow.swift
@@ -11,55 +11,83 @@ import SwiftUI
 struct CommentRow: View {
     var body: some View {
         
-        
-        ZStack {
-            
-           /*RoundedRectangle(cornerRadius: 15, style: .continuous)
-                .fill(Color.white)
-                .frame(height: 80)
-                .shadow(radius: 8)
-                .padding()
-            */
-            VStack {
-                HStack {
-                    Image("foto_leo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 40.0, height: 40.0)
-                        .clipShape(Circle())
-                        .padding(.leading)
-                    
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("Leonardo da Vinci")
-                                .font(.subheadline)
-                                .fontWeight(.bold)
-                                .lineLimit(1)
+        Text("Hello")
+        /*ZStack {
+                     
+                    VStack {
+                        VStack {
+                            HStack(alignment: .top) {
+                                Image(comentario.publicador.usuario.foto_perfil)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 40.0, height: 40.0)
+                                    .clipShape(Circle())
+                                    .padding(.leading)
+                                
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        Text(comentario.publicador.usuario.nome)
+                                            .font(.subheadline)
+                                            .fontWeight(.bold)
+                                            .lineLimit(1)
+                                            .frame(height: 20.0)
+                                        
+                                        Spacer()
+                                        Text(comentario.publicador.usuario.fluencia_ingles)
+                                            .foregroundColor(.gray)
+                                            .font(.footnote)
+                                            .lineLimit(1)
+                                        Circle()
+                                            .fill(comentario.publicador.usuario.cor_fluencia)
+                                            .frame(width: 10.0, height: 10.0)
+                                            .padding(.trailing)
+                                    }
+                                    Text("Apple Developer Academy | PUC-Rio | Brazil")
+                                        .frame(height: 6.0)
+                                        .font(.caption)
+                                        .foregroundColor(Color.gray)
+                                        .lineLimit(1)
+                                } //VStack
+                            } //HStack
                             
-                            Spacer()
-                            Text("English Level")
-                                .foregroundColor(.gray)
-                                .font(.footnote)
-                            Circle()
-                                .frame(width: 10.0, height: 10.0)
-                                .padding(.trailing)
-                        }
-                        Text("Apple Developer Academy | PUC-Rio | Brazil")
-                            .font(.caption)
-                            .foregroundColor(Color.gray)
-                            .lineLimit(1)
-                    } //VStack
-                } //HStack
-              
-                Text("We are the world, we are the children... we are the ones who make a brighter day, so lets got given. That's a choice we're making, to be saving our own lives.")
-                    .multilineTextAlignment(.leading)
-                        .padding(.horizontal)
-                        .frame(height: 100.0)
+                            
+                                
+                            HStack {
+                                Text(comentario.conteudo)
+                                    .font(.footnote)
+                                    .multilineTextAlignment(.leading)
+                                    .padding(.horizontal)
+                                    .frame(height: 40)
+                                    .background(Color.gray.opacity(0.15))
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                                
+                                Spacer()
+                            }
+                                
+                            
+                            
+                            
+                        } //VStack
                         
-            } //VStack
-            .padding(.horizontal)
-        }//ZStack
-    }
+                        
+                            HStack {
+                                Image("foto_victor")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 20.0, height: 20.0)
+                                    .clipShape(Circle())
+                                    .padding(.leading)
+                                
+                                TextField("Answer here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                                    .font(.footnote)
+                                
+                            } //HStack
+                            .padding(.horizontal)
+                    }
+                }//ZStack
+        .padding()
+            }*/
+}
 }
 
 struct CommentRow_Previews: PreviewProvider {
--- BarreirasLinguisticas/View/CommentView.swift
+++ BarreirasLinguisticas/View/CommentView.swift
@@ -12,8 +12,76 @@ struct CommentView: View {
     @ObservedObject var comentario: Comentario
     var body: some View {
         ZStack {
-            
-            
+            VStack {
+                VStack {
+                    HStack(alignment: .top) {
+                        Image(comentario.publicador.usuario.foto_perfil)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 40.0, height: 40.0)
+                            .clipShape(Circle())
+                            .padding(.leading)
+                        
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(comentario.publicador.usuario.nome)
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                    .lineLimit(1)
+                                    .frame(height: 20.0)
+                                
+                                Spacer()
+                                
+                                Text(comentario.publicador.usuario.fluencia_ingles)
+                                    .foregroundColor(.gray)
+                                    .font(.footnote)
+                                    .lineLimit(1)
+                                
+                                Circle()
+                                    .fill(comentario.publicador.usuario.cor_fluencia)
+                                    .frame(width: 10.0, height: 10.0)
+                                    .padding(.trailing)
+                            }
+                            Text("Apple Developer Academy | PUC-Rio | Brazil")
+                                .frame(height: 6.0)
+                                .font(.caption)
+                                .foregroundColor(Color.gray)
+                                .lineLimit(1)
+                        } //VStack
+                    } //HStack
+                    
+                    HStack {
+                        Text(comentario.conteudo)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal)
+                            .frame(height: 40)
+                            .background(Color.gray.opacity(0.1))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                        
+                        Spacer()
+                    }
+                } //VStack
+                
+                HStack {
+                    Image("foto_victor")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 20.0, height: 20.0)
+                        .clipShape(Circle())
+                        .padding(.leading)
+                    
+                    TextField("Answer here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .font(.footnote)
+                    
+                } //HStack
+                    .padding(.horizontal)
+            }
+        }//ZStack
+            .padding()
+    }
+    
+        /*ZStack {
             RoundedRectangle(cornerRadius: 15, style: .continuous)
                 .fill(Color.secondary)
                 .frame(height: 80)
@@ -53,9 +121,9 @@ struct CommentView: View {
             .padding(.horizontal)
         } //ZStack
     } //body
+}*/
+    
 }
-
-
 
 struct CommentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -63,3 +131,43 @@ struct CommentView_Previews: PreviewProvider {
     }
 }
 
+struct Topbar : View {
+    
+    @Binding var selected : Int
+    
+    var body : some View{
+        
+        HStack{
+            
+            Button(action: {
+                
+                self.selected = 0
+                
+            }) {
+                
+                Text("Questions")
+                    .frame(width: 25, height: 25)
+                    .background(self.selected == 0 ? Color.white : Color.clear)
+                    .clipShape(Capsule())
+            }
+            .foregroundColor(self.selected == 0 ? .pink : .gray)
+            
+            Button(action: {
+                
+                self.selected = 1
+                
+            }) {
+                
+                Text("Comments")
+                .frame(width: 25, height: 25)
+                .background(self.selected == 1 ? Color.white : Color.clear)
+                .clipShape(Capsule())
+            }
+            .foregroundColor(self.selected == 1 ? .pink : .gray)
+            
+            }.padding(8)
+            .background(Color.gray)
+            .clipShape(Capsule())
+            .animation(.default)
+    }
+}
